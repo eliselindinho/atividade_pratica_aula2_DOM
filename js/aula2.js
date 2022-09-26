e7709c7b937f83edee449e9f0c2711ce4c126e16
elis mendonca <elis_pm@hotmail.com>
2022-09-26 15:54:18 -0300

    Formatando html, exibindo no console quantos elementos filhos tem no body, exibindo true ou false de acordo com a quantidade de filhos no body e exibindo a nodeList

---
+++ js/aula2.js
@@ -0,0 +1,14 @@
+let titulo = (document.querySelector("#titulo").style.color = blue);
+
+console.log(document.body.childElementCount - 1);
+
+function paragrafo() {
+  if (document.body.childElementCount - 1 > 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(paragrafo());
+
+const myNodeList = document.querySelectorAll("div");
